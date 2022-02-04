76274c4ef409f62edec89587fd2eee9faef29427
DavidSM24 <david.dam.4.1.96@gmail.com>
2022-02-04 09:31:06 +0100

    en mitad del desarrollo

--- src/main/java/repositories/AgencyRepository.java
+++ src/main/java/repositories/AgencyRepository.java
@@ -1,9 +1,12 @@
 package repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import models.Agency;
 
 public interface AgencyRepository extends JpaRepository<Agency,Long>{
 
+	public List<Agency>getAllPaged(int offset, int page);
 }
