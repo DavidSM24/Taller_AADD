b31f37ec3b0cf81d6916299f4922ccca822636df
DavidSM24 <david.dam.4.1.96@gmail.com>
2022-02-04 19:19:04 +0100

    Subiendo agencia controller y service

--- src/main/java/repositories/AgencyRepository.java
+++ src/main/java/repositories/AgencyRepository.java
@@ -3,10 +3,14 @@ package repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import models.Agency;
 
 public interface AgencyRepository extends JpaRepository<Agency,Long>{
 
-	public List<Agency>getAllPaged(int offset, int page);
+	@Query(
+			  value = "SELECT * FROM agency limit 10,?", 
+			  nativeQuery = true)
+	public List<Agency>getAllPaged(int offset);
 }
