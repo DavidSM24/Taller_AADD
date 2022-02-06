7062a34ebe7f61168065f429373f2ef175fdd306
Jkinjt <l02jitoj@gmail.com>
2022-02-06 20:11:46 +0100

    CompanyRepository

--- src/main/java/repositories/InsuranceCompanyRepository.java
+++ src/main/java/repositories/InsuranceCompanyRepository.java
@@ -3,12 +3,18 @@ package repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import models.InsuranceCompany;
 @Repository
 public interface InsuranceCompanyRepository  extends JpaRepository<InsuranceCompany, Long>{
-	List<InsuranceCompany> getByNamePaged(String name);
-	List<InsuranceCompany> getByCIAName(String name);
+	@Query(value="SELECT * FROM insuranceCompany LIMIT 15 OFFSET :offset")
+	List<InsuranceCompany> getAllPaged(@Param("offset") int offset);
+	@Query(value = "SELECT * FROM insuranceCompany WHERE LOWER(name) LIKE '%:name%' LIMIT 15 OFFSET :offset")
+	List<InsuranceCompany> getByNamePaged(@Param("name") String name, @Param("offset") int offset);
+	@Query(value = "SELECT * FROM insuranceCompany WHERE LOWER(name) LIKE '%:name%'")
+	List<InsuranceCompany> getByCIAName(@Param("name") String name);
 
 }
