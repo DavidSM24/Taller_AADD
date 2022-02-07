fefa677c6a0e001396a66900d02a34e96fa9b625
Jkinjt <l02jitoj@gmail.com>
2022-02-07 09:00:57 +0100

    Subida para poder cambiar la paquetización

--- src/main/java/repositories/InsuranceCompanyRepository.java
+++ src/main/java/repositories/InsuranceCompanyRepository.java
@@ -12,8 +12,8 @@ import models.InsuranceCompany;
 public interface InsuranceCompanyRepository  extends JpaRepository<InsuranceCompany, Long>{
 	@Query(value="SELECT * FROM insuranceCompany LIMIT 15 OFFSET :offset")
 	List<InsuranceCompany> getAllPaged(@Param("offset") int offset);
-	@Query(value = "SELECT * FROM insuranceCompany WHERE LOWER(name) LIKE '%:name%' LIMIT 15 OFFSET :offset")
-	List<InsuranceCompany> getByNamePaged(@Param("name") String name, @Param("offset") int offset);
+	@Query(value = "SELECT * FROM insuranceCompany WHERE LOWER(name) LIKE '%:name%' LIMIT :limit OFFSET :offset")
+	List<InsuranceCompany> getByNamePaged(@Param("name") String name,@Param("limit") int limit, @Param("offset") int offset);
 	@Query(value = "SELECT * FROM insuranceCompany WHERE LOWER(name) LIKE '%:name%'")
 	List<InsuranceCompany> getByCIAName(@Param("name") String name);
 
