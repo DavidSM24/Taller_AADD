6eca81ba3a56c1b47433c10da31f82147282316d
Jkinjt <l02jitoj@gmail.com>
2022-02-08 19:19:59 +0100

    ¿Funcionara

--- src/main/java/project/models/InsuranceCompany.java
+++ src/main/java/project/models/InsuranceCompany.java
@@ -9,6 +9,7 @@ import javax.persistence.CascadeType;
 
 
 import javax.persistence.Column;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,14 +19,17 @@ import javax.persistence.OneToMany;
 @javax.persistence.Embeddable
 @javax.persistence.Table(name="insurancecompany")
 public class InsuranceCompany {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
-	protected Long id;
-	@Column(name="dateExchange",length = 50)
-	protected String CIA_Name;
-	@OneToMany(mappedBy = "agency", cascade = CascadeType.ALL, orphanRemoval = true)
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	protected Long id;
+	@Column(name = "dateExchange", length = 50)
+	protected String CIA_Name;
+
+	@OneToMany(mappedBy = "myInsurenceCompany", cascade = CascadeType.ALL, orphanRemoval = true,fetch =FetchType.LAZY)
 	protected List<Agency> agencies;
 	
 	public InsuranceCompany(Long id, String cIA_Name, List<Agency> agencies) {
