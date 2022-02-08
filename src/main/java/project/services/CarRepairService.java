f3fa62e1fd616c33e24f24eb55e4fcfb7d959212
Jkinjt <l02jitoj@gmail.com>
2022-02-08 19:36:06 +0100

    Inicio

--- src/main/java/project/services/CarRepairService.java
+++ src/main/java/project/services/CarRepairService.java
@@ -1,5 +1,16 @@
 package project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import project.repositories.CarRepairRepository;
+
+@Service
 public class CarRepairService {
+	
+	@Autowired
+	CarRepairRepository repository;
+	
+	
 
 }
