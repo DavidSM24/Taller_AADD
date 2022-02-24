717b0852a762d4fda38879020da892f42207da73
DavidSM24 <david.dam.4.1.96@gmail.com>
2022-02-24 20:09:58 +0100

    subiendo fix

--- src/main/java/project/services/GiftService.java
+++ src/main/java/project/services/GiftService.java
@@ -28,12 +28,12 @@ public class GiftService {
 	GiftRepository repository;
 
 	/**
-	 * Método que devuelve todos los regalos.
+	 * Mï¿½todo que devuelve todos los regalos.
 	 * 
 	 * @return lista de regalos.
 	 */
 	public List<Gift> getAll() {
-		logger.info("Petición realizada correctamente");
+		logger.info("Peticiï¿½n realizada correctamente");
 
 		return repository.findAll();
 	}
@@ -41,27 +41,27 @@ public class GiftService {
 	/**
 	 * Devuelve todas los regalos paginados empezando en la pagina recibida.
 	 * 
-	 * @param element nº de elementos a buscar
-	 * @param page    pagina por la que se comienza la paginación.
+	 * @param element nï¿½ de elementos a buscar
+	 * @param page    pagina por la que se comienza la paginaciï¿½n.
 	 * @return lista de regalos paginada.
 	 * @throws ServiceException
 	 */
 	public List<Gift> getAllPaged(int element, int page) throws ServiceException {
 		if (element > 0 && page > -1) {
-			logger.info("Petición realizada correctamente");
+			logger.info("Peticiï¿½n realizada correctamente");
 
 			return repository.getAllPaged(element, page);
 
 		} else {
-			logger.error("El número de elementos pedido no es válido");
+			logger.error("El nï¿½mero de elementos pedido no es vï¿½lido");
 
-			throw new ServiceException("El número de elementos pedido no es válido");
+			throw new ServiceException("El nï¿½mero de elementos pedido no es vï¿½lido");
 		}
 	}
 
 	/***
-	 * Método para conseguir un regalotir a partir de su id. Recibe un Long.
-	 * Posibilidad de dar una excepción NotFound.
+	 * Mï¿½todo para conseguir un regalotir a partir de su id. Recibe un Long.
+	 * Posibilidad de dar una excepciï¿½n NotFound.
 	 *
 	 *
 	 * @param id
@@ -74,7 +74,7 @@ public class GiftService {
 			Optional<Gift> result = repository.findById(id);
 
 			if (result.isPresent()) {
-				logger.info("Petición realizada correctamente");
+				logger.info("Peticiï¿½n realizada correctamente");
 
 				return result.get();
 
@@ -85,9 +85,9 @@ public class GiftService {
 			}
 
 		} else {
-			logger.error("La id introducida no es válida");
+			logger.error("La id introducida no es vï¿½lida");
 
-			throw new RecordNotFoundException("La id introducida no es válida");
+			throw new RecordNotFoundException("La id introducida no es vï¿½lida");
 		}
 
 	}
@@ -96,29 +96,29 @@ public class GiftService {
 	 * Devuelve una lista de regalos filtrada por el nombre recibido y paginada
 	 * comenzando por la pagina recivida.
 	 * 
-	 * @param name    nombre con el que se filtrará la búsqueda.
-	 * @param element nº de elementos a buscar
-	 * @param page    pagina por la que comienza la paginación de la lista.
+	 * @param name    nombre con el que se filtrarï¿½ la bï¿½squeda.
+	 * @param element nï¿½ de elementos a buscar
+	 * @param page    pagina por la que comienza la paginaciï¿½n de la lista.
 	 * @return lista de regalos filtrada por nombre y paginada.
 	 * @throws ServiceException
 	 */
 	public List<Gift> getByNamePaged(String name, int element, int page) throws ServiceException {
 		if (name != null && !name.equals("")) {
 			if (element > 0 && page > -1) {
-				logger.info("Petición realizada correctamente");
+				logger.info("Peticiï¿½n realizada correctamente");
 
 				return repository.getByNamePaged(name, element, page);
 
 			} else {
-				logger.error("El número de elementos pedido no es válido");
+				logger.error("El nï¿½mero de elementos pedido no es vï¿½lido");
 
-				throw new ServiceException("El número de elementos pedido no es válido");
+				throw new ServiceException("El nï¿½mero de elementos pedido no es vï¿½lido");
 			}
 
 		} else {
-			logger.error("El nombre introducido no es válido");
+			logger.error("El nombre introducido no es vï¿½lido");
 
-			throw new ServiceException("El nombre introducido no es válido");
+			throw new ServiceException("El nombre introducido no es vï¿½lido");
 		}
 	}
 
@@ -126,27 +126,27 @@ public class GiftService {
 	 * Devuelve una lista de regalos paginada filtrada por la disponibilidad actual,
 	 * comenzando por la pagina recivida.
 	 * 
-	 * @param avaliable disponibilidad para filtrar la búsqueda.
-	 * @param page      página para empezar la paginación en la búsqueda.
+	 * @param avaliable disponibilidad para filtrar la bï¿½squeda.
+	 * @param page      pï¿½gina para empezar la paginaciï¿½n en la bï¿½squeda.
 	 * @return lista de regalos paginada y filtrada por nombre
 	 * @throws ServiceException
 	 */
 	public List<Gift> getByAvaliablePaged(boolean avaliable, int element, int page) throws ServiceException {
 		if (element > 0 && page > -1) {
-			logger.info("Petición realizada correctamente");
+			logger.info("Peticiï¿½n realizada correctamente");
 
 			return repository.getByAvaliablePaged(avaliable, element, page);
 
 		} else {
-			logger.error("El número de elementos pedido no es válido");
+			logger.error("El nï¿½mero de elementos pedido no es vï¿½lido");
 
-			throw new ServiceException("El número de elementos pedido no es válido");
+			throw new ServiceException("El nï¿½mero de elementos pedido no es vï¿½lido");
 		}
 	}
 
 	/***
-	 * Método para insertar o actualizar un regalo dependiendo de si existe un
-	 * registro con este id en la BBDD. Lanza una excepción si no se encuentra el
+	 * Mï¿½todo para insertar o actualizar un regalo dependiendo de si existe un
+	 * registro con este id en la BBDD. Lanza una excepciï¿½n si no se encuentra el
 	 * regalo en la BBDD.
 	 * 
 	 * @param Gift: El regalo a actualizar/insertar.
@@ -155,69 +155,68 @@ public class GiftService {
 	 * @throws ServiceException
 	 */
 	public Gift createOrUpdate(Gift gift, MultipartFile file) throws RecordNotFoundException, ServiceException {
-		if(gift!=null) {
+		if (gift != null) {
 			if (gift.getId() != null && gift.getId() > 0) {
 				Optional<Gift> a = repository.findById(gift.getId());
-				
+
 				if (a.isPresent()) { // update
 					Gift newGift = a.get();
-					
-					CloudinaryRepository.delete(newGift.getPicture()); //elimina la anterior foto.
-					
+
+					CloudinaryRepository.delete(newGift.getPicture()); // elimina la anterior foto.
+
 					newGift.setId(gift.getId());
 					newGift.setName(gift.getName());
 					newGift.setPoints(gift.getPoints());
 					newGift.setAvailable(gift.isAvailable());
-					newGift.setPicture(CloudinaryRepository.upload(file, newGift)); //crea una nueva.
+					newGift.setPicture(CloudinaryRepository.upload(file, newGift)); // crea una nueva.
 					newGift.setExchangeGifts(gift.getExchangeGifts());
-					
-					newGift=repository.save(newGift);
-					logger.info("Petición realizada correctamente");
-					
+
+					newGift = repository.save(newGift);
+					logger.info("Peticiï¿½n realizada correctamente");
+
 					return newGift;
-					
+
 				} else { // insert
 					gift.setId(null);
 					gift.setPicture(CloudinaryRepository.upload(file, gift));
 					gift = repository.save(gift);
-					logger.info("Petición realizada correctamente");
-					
-					
-					
-				}else {
-					gift.setPicture(CloudinaryRepository.upload(file, gift));
-					gift=repository.save(gift);
-					logger.info("Petición realizada correctamente");
-					
-					return gift;
+					logger.info("Peticiï¿½n realizada correctamente");
+
 				}
-			
-		}else {
+			} else {
+				gift.setPicture(CloudinaryRepository.upload(file, gift));
+				gift = repository.save(gift);
+				logger.info("Peticiï¿½n realizada correctamente");
+
+				return gift;
+			}
+
+		} else {
 			logger.error("El regalo introducido es nulo");
-			
 			throw new ServiceException("El regalo introducido es nulo");
+			
 		}
+		return null;
 	}
 
 	/***
-	 * Método que recibe un regalo y lo elimina de la BBDD. Lanza una excepción si
+	 * Mï¿½todo que recibe un regalo y lo elimina de la BBDD. Lanza una excepciï¿½n si
 	 * no se encuentra el regalo en la BBDD.
 	 * 
 	 * @param: Gift El regalo a eliminar.
 	 * @throws RecordNotFoundException
 	 * @throws ServiceException
 	 */
-	public void delete(Gift gift) throws RecordNotFoundException, ServiceException{
-		if(gift!=null) {
-			if(gift.getId()!=null) {
-				if(gift.getId()>0) {
-					Optional<Gift> optional=repository.findById(gift.getId());
-					
-					if(optional.isPresent()) {
+	public void delete(Gift gift) throws RecordNotFoundException, ServiceException {
+		if (gift != null) {
+			if (gift.getId() != null) {
+				if (gift.getId() > 0) {
+					Optional<Gift> optional = repository.findById(gift.getId());
+
+					if (optional.isPresent()) {
 						CloudinaryRepository.delete(gift.getPicture());
 
-						logger.info("Petición realizada correctamente");
-
+						logger.info("Peticiï¿½n realizada correctamente");
 
 						repository.deleteById(gift.getId());
 					} else {
@@ -233,7 +232,7 @@ public class GiftService {
 				}
 
 			} else {
-				logger.error("El id introducido no es válido");
+				logger.error("El id introducido no es vï¿½lido");
 
 				throw new RecordNotFoundException("El id introducido es invalido", gift.getId());
 			}
